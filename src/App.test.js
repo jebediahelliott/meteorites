b869485bfd4fdfe197b7981e62b838a3edf39de4
jebediahelliott <jebediahelliott@gmail.com>
2019-05-18 20:06:30 -0400

    Change initial App configuration. Add enzyme setup. Change default test.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+  it('renders a title', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('h1').text()).toEqual('Meteorite Explorer')
+  });
 });
