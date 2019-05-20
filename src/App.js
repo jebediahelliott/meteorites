5b6fea39c4244076bec380b798b04f7bac907368
jebediahelliott <jebediahelliott@gmail.com>
2019-05-19 23:40:09 -0400

    Add test for API call and implement. Switch App to class based component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Meteorite Explorer</h1>
-      </header>
-      <body className="App-body">
-        <SearchBar />
-      </body>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      meteorites: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://data.nasa.gov/resource/gh4g-9sfh.json')
+    .then(resp => console.log(resp))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Meteorite Explorer</h1>
+        </header>
+        <body className="App-body">
+          <SearchBar />
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import App from './App';
-import { shallow } from 'enzyme'
+import { shallow } from 'enzyme';
+import axios from 'axios';
+// jest.mock('axios')
+
 
 describe('<App />', () => {
   it('renders a title', () => {
     const wrapper = shallow(<App />)
     expect(wrapper.find('h1').text()).toEqual('Meteorite Explorer')
   });
+  it('fetches the meteorite list when it mounts', () => {
+    const spy = jest.spyOn(axios, 'get');
+    const wrapper = shallow(<App />);
+    spy.mockResolvedValue(42);
+    expect(spy).toHaveBeenCalledWith('https://data.nasa.gov/resource/gh4g-9sfh.json');
+  })
 });
