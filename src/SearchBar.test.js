7f59f6143133ccdb2c856e6c2e0a7a9f5b203171
jebediahelliott <jebediahelliott@gmail.com>
2019-05-20 11:48:47 -0400

    Add test for submission in SearchBar

--- src/SearchBar.test.js
+++ src/SearchBar.test.js
@@ -11,4 +11,14 @@ describe('<SearchBar />', () => {
     const wrapper = shallow(<SearchBar />);
     expect(wrapper.find('button').length).toEqual(1);
   });
+  it('uses the search term to make a request to the API', () => {
+    const wrapper = shallow(<SearchBar />);
+    const instance = wrapper.instance();
+    const spy = jest.spyOn(instance, 'search');
+    const input = wrapper.find('input');
+    const button = wrapper.find('button');
+    input.simulate('change', {target: {value: 'search term'}})
+    button.simulate('click');
+    expect(spy).toHaveBeenCalled();
+  })
 });
