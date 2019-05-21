53f0b1cf257ef0d1e3138b8656c56bc89343ef17
jebediahelliott <jebediahelliott@gmail.com>
2019-05-21 11:52:25 -0400

    Add message for search with no results

--- src/MeteoriteList.js
+++ src/MeteoriteList.js
@@ -22,28 +22,38 @@ class MeteoriteList extends Component {
   }
 
   render() {
-    return (
-      <div className="MeteoriteList">
-        <table>
-          <thead>
-            <tr id="theader">
-              <th>Name</th>
-              <th>Id</th>
-              <th>Name Type</th>
-              <th>Rec Class</th>
-              <th>Mass(g)</th>
-              <th>Fall</th>
-              <th>Year</th>
-              <th>Latitude</th>
-              <th>Longitude</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.rows()}
-          </tbody>
-        </table>
-      </div>
-    )
+    console.log(this.props.meteorites.lenght);
+    if (this.props.meteorites.length === 0) {
+      return (
+        <div className="noResult">
+          <p>Sorry, there were no results that match your search. Please try again.</p>
+        </div>
+      )
+    }else {
+      return (
+        <div className="MeteoriteList">
+          <table>
+            <thead>
+              <tr id="theader">
+                <th>Name</th>
+                <th>Id</th>
+                <th>Name Type</th>
+                <th>Rec Class</th>
+                <th>Mass(g)</th>
+                <th>Fall</th>
+                <th>Year</th>
+                <th>Latitude</th>
+                <th>Longitude</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.rows()}
+            </tbody>
+          </table>
+        </div>
+      )
+    }
+
   }
 }
 
