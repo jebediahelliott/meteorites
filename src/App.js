619ea5ad8bff720abbf33b1298fd0a382e787ad7
jebediahelliott <jebediahelliott@gmail.com>
2019-05-27 15:09:13 -0400

    Add tests and implementation for alphabetizing API results

--- src/App.js
+++ src/App.js
@@ -14,17 +14,33 @@ class App extends Component {
 
   defaultRequest = () => {
     axios.get('https://data.nasa.gov/resource/gh4g-9sfh.json')
-    .then(resp => this.setState({meteorites: resp.data}));
+    .then(resp => this.setState({meteorites: this.alphabetize(resp.data)}));
   }
 
   parseQuery = term => {
     return term.replace(/'/g, "''").toLowerCase()
   }
 
+  alphabetize = (dataArray) => {
+    return dataArray.sort(function(a, b) {
+      const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+      const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+
+      // names must be equal
+      return 0;
+    })
+  }
+
   queryNASA = term => {
     axios.get(`https://data.nasa.gov/resource/gh4g-9sfh.json?$where=lower(name) like '%25${this.parseQuery(term)}%25'`)
     .then(resp => {
-      this.setState({meteorites: resp.data})
+      this.setState({meteorites: this.alphabetize(resp.data)})
     })
     .catch(res => console.log(res.message));
   }
--- src/App.test.js
+++ src/App.test.js
@@ -24,13 +24,13 @@ describe('<App />', () => {
     expect(spy).toHaveBeenCalledWith('https://data.nasa.gov/resource/gh4g-9sfh.json');
   });
   it('sets state with response from NASA API', async () => {
-    const resp = {data: 42}
+    const resp = {data: [{name: "Ouzina", id: "18054", nametype: "Valid", recclass: "R4", mass: "642"}]}
     const spy = jest.spyOn(axios, 'get');
     spy.mockResolvedValue(resp);
     const wrapper = shallow(<App />);
     const instance = wrapper.instance();
-    await instance.componentDidMount()
-    expect(wrapper.state('meteorites')).toEqual(42);
+    await instance.componentDidMount();
+    expect(wrapper.state('meteorites')).toEqual([{name: "Ouzina", id: "18054", nametype: "Valid", recclass: "R4", mass: "642"}]);
   });
   it('parses user search term', () => {
     const wrapper = shallow(<App />);
@@ -42,8 +42,6 @@ describe('<App />', () => {
     expect(instance.parseQuery(searchTerm1)).toEqual("''adfaf@$%@%2454''")
     expect(instance.parseQuery(searchTerm2)).toEqual('yabba dabba doo')
     expect(instance.parseQuery(searchTerm3)).toEqual("''the k!t<|-|en $ink''")
-
-
   })
   it('queries with a search term when a search term is given', () => {
     const wrapper = shallow(<App />);
@@ -52,5 +50,49 @@ describe('<App />', () => {
     const searchTerm = 'Meteorite'
     instance.queryNASA(searchTerm)
     expect(spy).toHaveBeenLastCalledWith(`https://data.nasa.gov/resource/gh4g-9sfh.json?$where=lower(name) like '%25meteorite%25'`)
+  })
+  it('alphabetizes results based on name', async () => {
+    const sortedResults = [
+      {name: "Bi'r Tazit", id: "48983", nametype: "Valid", recclass: "CK6", mass: "46"},
+      {name: "Ilizi", id: "35519", nametype: "Valid", recclass: "H4", mass: "185"},
+      {name: "Jodzie", id: "12173", nametype: "Valid", recclass: "Howardite", mass: "30"},
+      {name: "Maziba", id: "15454", nametype: "Valid", recclass: "L6", mass: "4975"},
+      {name: "McKenzie Draw (a)", id: "15461", nametype: "Valid", recclass: "H4", mass: "11800"},
+      {name: "McKenzie Draw (b)", id: "15462", nametype: "Valid", recclass: "H4", mass: "2990"},
+      {name: "Menziswyl", id: "15486", nametype: "Valid", recclass: "L5", mass: "28.9"},
+      {name: "New Leipzig", id: "16955", nametype: "Valid", recclass: "Iron, IAB-MG", mass: "20000"},
+      {name: "Ouzina", id: "18054", nametype: "Valid", recclass: "R4", mass: "642"},
+      {name: "Podgrodzie", id: "31301", nametype: "Valid", recclass: "H4/5", mass: "8.9"},
+      {name: "Santa Luzia", id: "23166", nametype: "Valid", recclass: "Iron, IIAB", mass: "1918000"},
+      {name: "Zaborzika", id: "30379", nametype: "Valid", recclass: "L6", mass: "3867"},
+      {name: "Zakłodzie", id: "30390", nametype: "Valid", recclass: "Enst achon-ung", mass: "8680"},
+      {name: "Zillah 001", id: "31355", nametype: "Valid", recclass: "L6", mass: "1475"},
+      {name: "Zillah 002", id: "31356", nametype: "Valid", recclass: "Eucrite", mass: "172"},
+      {name: "Zinder", id: "30409", nametype: "Valid", recclass: "Pallasite, ungrouped", mass: "46"}
+    ];
+    const unsortedResults = [
+      {name: "Ouzina", id: "18054", nametype: "Valid", recclass: "R4", mass: "642"},
+      {name: "Zinder", id: "30409", nametype: "Valid", recclass: "Pallasite, ungrouped", mass: "46"},
+      {name: "Ilizi", id: "35519", nametype: "Valid", recclass: "H4", mass: "185"},
+      {name: "Jodzie", id: "12173", nametype: "Valid", recclass: "Howardite", mass: "30"},
+      {name: "Zakłodzie", id: "30390", nametype: "Valid", recclass: "Enst achon-ung", mass: "8680"},
+      {name: "McKenzie Draw (a)", id: "15461", nametype: "Valid", recclass: "H4", mass: "11800"},
+      {name: "Santa Luzia", id: "23166", nametype: "Valid", recclass: "Iron, IIAB", mass: "1918000"},
+      {name: "McKenzie Draw (b)", id: "15462", nametype: "Valid", recclass: "H4", mass: "2990"},
+      {name: "Zillah 001", id: "31355", nametype: "Valid", recclass: "L6", mass: "1475"},
+      {name: "Menziswyl", id: "15486", nametype: "Valid", recclass: "L5", mass: "28.9"},
+      {name: "New Leipzig", id: "16955", nametype: "Valid", recclass: "Iron, IAB-MG", mass: "20000"},
+      {name: "Podgrodzie", id: "31301", nametype: "Valid", recclass: "H4/5", mass: "8.9"},
+      {name: "Bi'r Tazit", id: "48983", nametype: "Valid", recclass: "CK6", mass: "46"},
+      {name: "Maziba", id: "15454", nametype: "Valid", recclass: "L6", mass: "4975"},
+      {name: "Zaborzika", id: "30379", nametype: "Valid", recclass: "L6", mass: "3867"},
+      {name: "Zillah 002", id: "31356", nametype: "Valid", recclass: "Eucrite", mass: "172"}
+    ];
+    const spy = jest.spyOn(axios, 'get');
+    spy.mockResolvedValue({data: unsortedResults});
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+    await instance.queryNASA('zi');
+    expect(wrapper.state('meteorites')).toEqual(sortedResults);
   })
 });
